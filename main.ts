7d497b4b7f1e643a04a5b1b2c22428dce7faa912
jaymeramsay <jayme.ramsay@gmail.com>
2018-05-17 17:39:45 -0500

    attempts to create variables in typescript

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 var canvas: HTMLCanvasElement;
 var context: CanvasRenderingContext2D;
 
+let dataPoints: string[] = []
+
 window.onload = () => {
   canvas = <HTMLCanvasElement>document.querySelector('canvas');
   context = canvas.getContext('2d');
